a9fdea770aa0a458494aa51235cbabc2c0c06db9
Florian Mros <48620727+floork@users.noreply.github.com>
2022-03-13 17:13:03 +0100

    fixed powerlevel10k

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 #install zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -9,28 +7,38 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh//plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-#starship theme
-git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+#powerlevel10k theme
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 
-cat <<EOF > ~/.zshrc
-export ZSH="$HOME/.oh-my-zsh"
+cat << EOF > ~/.zshrc
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export ZSH="/home/floork/.oh-my-zsh"
+
+# theme
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 #plugins
 plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
-source $ZSH/oh-my-zsh.sh
+source /home/floork/.oh-my-zsh/oh-my-zsh.sh
 
 # Load aliases and shortcuts if existent.
-[ -f "$HOME/.zsh/aliasrc" ] && source "$HOME/.zsh/aliasrc"
+[ -f "/home/floork/.zsh/aliasrc" ] && source "/home/floork/.zsh/aliasrc"
 
-#theme
-ZSH_THEME="powerlevel10k/powerlevel10k"
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 EOF
 
 echo "exec zsh" | sudo tee -a ~/.bashrc
 
-sudo mkdir ~/.zsh
-sudo cp ~/zsh/aliasrc ~/.zsh
+mkdir ~/.zsh
+cp ~/zsh/aliasrc ~/.zsh
 
 #make default shell
  chsh -s /bin/zsh
