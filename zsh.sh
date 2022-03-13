61cff2699c2127baf2a68eafa98afab843bd78ef
Florian Mros <48620727+floork@users.noreply.github.com>
2022-03-13 16:27:55 +0100

    EOF is better

--- zsh.sh
+++ zsh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 #install zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -11,17 +13,19 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 
-echo 'export ZSH="$HOME/.oh-my-zsh"' | sudo tee ~/.zshrc
-echo "" | sudo tee -a ~/.zshrc
-echo "#plugins" | sudo tee -a ~/.zshrc
-echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" | sudo tee -a ~/.zshrc
-echo 'source $ZSH/oh-my-zsh.sh' | sudo tee -a ~/.zshrc
-echo "" | sudo tee -a ~/.zshrc
-echo "# Load aliases and shortcuts if existent." | sudo tee -a ~/.zshrc
-echo '[ -f "$HOME/.zsh/aliasrc" ] && source "$HOME/.zsh/aliasrc"' | sudo tee -a ~/.zshrc
-echo "" | sudo tee -a ~/.zshrc
-echo "#theme" | sudo tee -a ~/.zshrc
-echo 'eval $(starship init zsh)' | sudo tee -a ~/.zshrc
+cat cat <<EOF > ~/.zshrc
+export ZSH="$HOME/.oh-my-zsh"
+
+#plugins
+plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+source $ZSH/oh-my-zsh.sh
+
+# Load aliases and shortcuts if existent.
+[ -f "$HOME/.zsh/aliasrc" ] && source "$HOME/.zsh/aliasrc"
+
+#theme
+eval "$(starship init zsh)"
+EOF
 
 echo "exec zsh" | sudo tee -a ~/.bashrc
 
